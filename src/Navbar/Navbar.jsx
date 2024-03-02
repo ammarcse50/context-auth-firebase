3dc76602cb33bd959618145c16d24b9cc5e29043
ammmar <ammaraslam7164@gmail.com>
2024-03-02 22:59:02 +0600

    gmail-sign-in-out-v1

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,56 +1,72 @@
-import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import { AuthContext } from '../providers/AuthProvider';
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../providers/AuthProvider";
 
 const Navbar = () => {
+  const { user, logOut } = useContext(AuthContext);
 
-         const {user,logOut} = useContext(AuthContext)
+  const handleLogOut = () => {
+    logOut()
+      .then(() => {
+        console.log("successfully logout");
+      })
+      .catch();
+  };
 
-  
-       const handleLogOut = ()=>{
-
-            logOut()
-            .then(()=>{
-              console.log('successfully logout')
-            })
-            .catch()
-
-       }
-
-    return (
-        <div>
-            <div className="navbar bg-base-100">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-      </div>
-     
-    </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-      <li><Link to="/home">Home</Link></li>
-      <li>
-       <Link to="/login">Login</Link>
-      </li>
-      <li><Link to="/register">Register</Link></li>
-    </ul>
-  </div>
-  <div className="navbar-end">
-    {
-
-          user && <span>
-            {user.email}
-          </span>
-
-    }
- <a onClick={handleLogOut} className="btn">Sign Out</a>
-  </div>
-</div>
+  return (
+    <div>
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+          </div>
+          <a className="btn btn-ghost text-xl">daisyUI</a>
         </div>
-    );
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <Link to="/home">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end mr-3">
+          {user ? (
+            <>
+              <span className="mr-4">{user.email}</span>
+              <a onClick={handleLogOut} className="btn">
+                Sign Out
+              </a>
+            </>
+          ) : (
+            <Link to="/login" className="btn">
+              Sign In
+            </Link>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
